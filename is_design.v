0c05a242c854e3b3cc95f12005ac339a82dc6945
Dharineesh0510 <thangaveldharineesh3@gmail.com>
2025-08-15 18:14:07 +0530

    Exercise 2.8

---
+++ is_design.v
@@ -0,0 +1,21 @@
+
+module MEM;  
+endmodule  
+
+module SC;  
+endmodule  
+
+module Xbar;  
+endmodule  
+
+
+module IS;  
+    MEM mem1();  
+    SC sc1();  
+    Xbar xbar1();  
+endmodule  
+
+
+module Top;  
+    IS is1();  
+endmodule  
